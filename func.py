0369d26438e698547a9ab0c8a2208c524f5575fa
jaag5678 <akshayg95@gmail.com>
2016-04-14 12:54:16 +0530

    Minor Changes

--- func.py
+++ func.py
@@ -9,14 +9,66 @@ def declaresynerror():
 	sys.exit("Check your input")
 	
 def printstate():
+	print "Reg : ",
 	for item in reg:
 		print item.val,
-	print ""
+	print "\n"
+	print "Stack : ",
 	for item in stack:
 		print item
+	print "\n"
+	print "Flags : ",
+	print flags.b.overflow,
+	print flags.b.direction,
+	print flags.b.interrupt,
+	print flags.b.trap,
+	print flags.b.sign,
+	print flags.b.zero,
+	print flags.b.ac,
+	print flags.b.parity,
+	print flags.b.carry
+	print "\n"
+
+
+#Wont work Cause it takes care only wen positive are arguments as operations
+#This func can be used only for INC operation	
+def checkoverflow(key):
+	if(key < 9) :
+		#pass
+		#FOR Overflow					#push this in update()
+		if(reg[key].val == 128) :
+			flags.b.overflow = 1			#this is Akshay G
+	elif(reg[key].val == 32768) :
+		flags.b.overflow = 1 	
+	if(reg[key].val == 0) :
+		flags.b.carry = 1 #Not always for zero
+		flags.b.zero  = 1 
+		flags.b.overflow = 1 
+
+#Genereic func for every operation Except wen the op does involve and		
+def updateacflag(temp):
+	ac = 0
+	temp -= 1
+	for i in range(4) :
+		if(temp & 1 == 1) :
+			ac = ac + 1
+		temp = temp >> 1	
+	if(ac == 4) :
+		flags.b.ac = 1 						
+ 	return
+ 	
+def updatedep(plan):
+	if(plan <= 2):
+		ax.update()
+	elif(plan <= 4 and plan > 2):
+		bx.update()
+	elif(plan <= 6 and plan > 4):
+		cx.update()
+	elif(plan <= 8 and plan > 6):
+		dx.update()
 		
 def extract(string):
-	#MODE 0 -> Registere
+	#MODE 0 -> Register
 	#MODE 1 -> Literal value
 	if('$' in string):
 		#convert into decimal or convert decimal to hex
@@ -45,13 +97,11 @@ def fmov(dest , source):
 		pass
 	if(modes == 1 and moded == 0):
 		reg[lookup3[vald]].val = vals
-	
-	if(lookup3[vald] < 10) :
+	if(lookup3[vald] < 10):
 		reg[lookup3[vald]].update()
-	else :
+		updatedep(lookup3[vald])
+	else:
 		reg[lookup3[vald]].valoverride(reg[lookup3[vald]].val)		
-	
-		
 	return
 		
 def finc(dest):						#manage ax, bx, cx, dx in another list, split values? hex?
@@ -62,47 +112,19 @@ def finc(dest):						#manage ax, bx, cx, dx in another list, split values? hex?
 	reg[lookup3[vald]].val += 1
 	if(lookup3[vald] < 9) :
 		reg[lookup3[vald]].update()
+		updatedep(lookup3[vald])
 	else : 
 		reg[lookup3[vald]].valoverride(reg[lookup3[vald]].val)	
 	
-	#Updating the Flag Register
-	if(lookup3[vald] < 9) :
-		#pass
-		#FOR Overflow
-		if(reg[lookup3[vald]].val == 128) :
-			flags.b.overflow = 1
-			print "Overflow occured Oerflow bit = ", flags.b.overflow
-	elif(reg[lookup3[vald]].val == 32768) :
-		flags.b.overflow = 1 	
-		print "Overflow occured Oerflow bit = ", flags.b.overflow	
-	if(reg[lookup3[vald]].val == 0) :
-		flags.b.carry = 1
-		flags.b.zero  = 1 
+	#Updating the Flag Register, do for overflow
+	#For overflow
+	if(reg[lookup3[vald]].val == 128 or reg[lookup3[vald]].val == 32768 or 
+		reg[lookup3[vald]].val == 0) :
 		flags.b.overflow = 1
-		print "Carry gen", flags.b.carry
-		print "VAlue in reg is zero", flags.b.zero
-		print "Overflow occured Oerflow bit = ", flags.b.overflow
-	temp = reg[lookup3[vald]].val
-	c = 0
-	ac = 0
-	while (temp > 0) :
-		if(temp & 1 == 1):
-			c = c + 1
-		temp = temp >> 1
-	if(c % 2 == 0) :
-		flags.b.parity = 1
-		print "Parity even so Parity bit set to 1", flags.b.parity
-						
-	temp = reg[lookup3[vald]].val - 1
-	for i in range(4) :
-		if(temp & 1 == 1) :
-			ac = ac + 1
-		temp = temp >> 1	
-	if(ac == 4) :
-		flags.b.ac = 1
-		print "Auxiallary Carry ocuured" 						
- 			
-	return
+	
+	if(lookup3[vald] > 16):
+		updateacflag(reg[lookup3[vald]].val)
+	
 	
 def fpop(dest):
 	(vald, moded) = extract(dest)
@@ -112,7 +134,8 @@ def fpop(dest):
 	if(lookup3[vald] < 13):
 		print "Cant pop 2 bytes in 1 byte register"
 		declaresynerror()
-	reg[lookup3[vald]].val = stack.pop() 
+	reg[lookup3[vald]].val = stack.pop()
+	reg[lookup3[vald]].valoverride(reg[lookup3[vald]].val)
 	return
 	
 def fpush(source):
@@ -127,7 +150,7 @@ def fpush(source):
 	return
 	
 def fxor(dest, source):
-	(vald, moded) = extract(dest)
+	(vald, moded) = extract(dest)		#check if xor ax, 05 is allowed
 	(vals, modes) = extract(source)
 	if(modes == 1 and moded == 1):
 		declaresynerror()
@@ -135,31 +158,27 @@ def fxor(dest, source):
 	#do other cases
 	if(modes == 0 and moded == 0):
 		if((lookup3[vald] < 10 and lookup3[vals] < 10) or (lookup3[vald] > 16 and lookup3[vals] > 16)) :
+			temp_p = reg[lookup3[vald]].val
+			temp_a = reg[lookup3[vals]].val
 			reg[lookup3[vald]].val ^= reg[lookup3[vals]].val
-		else: 
+		else: 	
+			print "type mis-match"
 			declaresynerror()
-	if(lookup3[vald] < 10):
-		pass
-	else: 
-		reg[lookup3[vald]].valoverride(reg[lookup3[vald]].val)			
-				
-	
-	temp = reg[lookup3[vald]].val
-	c = 0
-	ac = 0
-	while (temp > 0) :
-		if(temp & 1 == 1):
-			c = c + 1
-		temp = temp >> 1
-	if(c % 2 == 0) :
-		flags.b.parity = 1
-		print "Parity even so Parity bit set to 1", flags.b.parity
-	if(reg[lookup3[vald]].val == 0):
-		flags.b.zero = 1
-		print "Value is zero"		
-		
-	
-		
+			return	#imp here
+		if(lookup3[vald] < 10):
+			reg[lookup3[vald]].update()
+			updatedep(lookup3[vald])
+		else:
+			reg[lookup3[vald]].valoverride(reg[lookup3[vald]].val)
+		#overflowcheck()
+	if(lookup3[vald] < 10) :
+		if((temp_p > 127 and temp_a > 127) or (temp_p <= 127 and temp_a > 127) or (temp_p > 127 and temp_a <= 127)):
+			flags.b.overflow = 1
+	else:		
+		if ((temp_p > 32767 and temp_a > 32767) or (temp_p <= 32767 and temp_a > 32767) or(temp_p > 32767 and temp_a <= 32767)):
+			flags.b.overflow = 1	
+			
+							
 def blank():
 	pass
 	
@@ -178,6 +197,14 @@ def fxchg(dest, source):
 		reg[lookup3[vald]].val ^= reg[lookup3[vals]].val
 		reg[lookup3[vals]].val ^= reg[lookup3[vald]].val
 		reg[lookup3[vald]].val ^= reg[lookup3[vals]].val
+		if(lookup3[vald] < 10):	#both or none
+			reg[lookup3[vald]].update()
+			updatedep(lookup3[vald])
+			reg[lookup3[vals]].update()
+			updatedep(lookup3[vals])
+		else:
+			reg[lookup3[vald]].valoverride(reg[lookup3[vald]].val)
+			reg[lookup3[vals]].valoverride(reg[lookup3[vals]].val)
 		return
 def fmul(source):
 	a = 17
@@ -220,18 +247,46 @@ def fnot(dest):
 		reg[lookup3[vald]].val ^= 15
 	elif(reg[lookup3[vald]].val <= 255):
 		reg[lookup3[vald]].val ^= 255
+	if(lookup3[vald] < 13):
+		reg[lookup3[vald]].update()
+		updatedep(lookup3[vald])
+	else:
+		reg[lookup3[vald]].valoverride(reg[lookup3[vald]].val)
 	#add more or go sustainable
 	
 def fbor(dest, source):
 	(vald, moded) = extract(dest)
 	(vals, modes) = extract(source)
-	if(modes == 1 and moded == 1):
+	if(moded == 1):
 		declaresynerror()
 		return
 	#do other cases
 	if(modes == 0 and moded == 0):
+		if((lookup3[vals] < 13 and lookup3[vald] >= 13) or (lookup3[vald] < 13 and lookup3[vals] >= 13)):
+			declaresynerror()
+			print "Size mis-match"
+			return
+		temp_p = reg[lookup3[vald]].val
+		temp_a = reg[lookup3[vals]].val
 		reg[lookup3[vald]].val |= reg[lookup3[vals]].val
-		return
+	elif(moded == 0) :
+		temp_p = reg[lookup3[vald]].val
+		temp_a = vals
+		reg[lookup3[vald]].val |= vals		
+	if(lookup3[vald] < 10):
+		reg[lookup3[vald]].update()
+		updatedep(lookup3[vald])
+	else:
+		reg[lookup3[vald]].valoverride(reg[lookup3[vald]].val)
+	#For Overflow
+	if(lookup3[vald] < 10):
+		if((temp_p > 127 and temp_a <= 127) or (temp_p <= 127 and temp_a > 127)):
+			flags.b.overflow = 1
+	else:
+		if((temp_p > 32767 and temp_a <= 32767) or (temp_p <= 32767 and temp_a > 32767)):
+			flags.b.overflow = 1		
+			
+	return
 		
 def fand(dest, source):
 	(vald, moded) = extract(dest)
@@ -241,21 +296,16 @@ def fand(dest, source):
 		return
 	#do other cases
 	if(modes == 0 and moded == 0):
+		if((lookup3[vals] < 13 and lookup3[vald] >= 13) or (lookup3[vald] < 13 and lookup3[vals] >= 13)):
+			declaresynerror()
+			print "Size mis-match"
+			return
 		reg[lookup3[vald]].val &= reg[lookup3[vals]].val
-		
-	temp = reg[lookup3[vald]].val
-	c = 0
-	ac = 0
-	while (temp > 0) :
-		if(temp & 1 == 1):
-			c = c + 1
-		temp = temp >> 1
-	if(c % 2 == 0) :
-		flags.b.parity = 1
-		print "Parity even so Parity bit set to 1", flags.b.parity
-	if(reg[lookup3[vald]].val == 0):
-		flags.b.zero = 1
-		print "Value is zero"
+	if(lookup3[vald] < 13):
+		reg[lookup3[vald]].update()
+		updatedep(lookup3[vald])
+	else:
+		reg[lookup3[vald]].valoverride(reg[lookup3[vald]].val)
 				
 		
 def fdec(dest):
@@ -263,60 +313,21 @@ def fdec(dest):
 	if(moded == 1):
 		declaresynerror()
 		return
-	#if(reg[lookup3[vald]].val == 0):
-	#	if(lookup3[vald] >= 16):
-	#		reg[lookup3[vald]].val = 65535
-	#		declareoverflow()
-	#		return
-	#	if(lookup3[vald] < 13):
-	#		reg[lookup3[vald]].val = 255
-	#		declareoverflow()
-	#		return
 	reg[lookup3[vald]].val -= 1
+	if(lookup3[vald] < 13):
+		reg[lookup3[vald]].update
+		updatedep(lookup3[vald])
+	else:
+		reg[lookup3[vald]].valoverride(reg[lookup3[vald]].val)
+	#checkoverflow(lookup3[vald])
+	if(lookup3[vald] > 16):
+		updateacflag(reg[lookup3[vald]].val)
 	
-	#Updating the Flag Register
-	if(lookup3[vald] < 9) :
-		#pass
-		#FOR Overflow
-		if(reg[lookup3[vald]].val == 127) :
-			flags.b.overflow = 1
-			print "Overflow occured Oerflow bit = ", flags.b.overflow
-	elif(reg[lookup3[vald]].val == 32767) :
-		flags.b.overflow = 1 	
-		print "Overflow occured Oerflow bit = ", flags.b.overflow	
-	if(reg[lookup3[vald]].val == -1) :
-		flags.b.carry = 1
-		flags.b.overflow = 1
-		print "Carry gen", flags.b.carry
-		print "VAlue in reg is zero", flags.b.zero
-		print "Overflow occured Oerflow bit = ", flags.b.overflow
-	temp = reg[lookup3[vald]].val
-	c = 0
-	ac = 0
-	while (temp > 0) :
-		if(temp & 1 == 1):
-			c = c + 1
-		temp = temp >> 1
-	if(c % 2 == 0) :
-		flags.b.parity = 1
-		print "Parity even so Parity bit set to 1", flags.b.parity
-						
-	temp = reg[lookup3[vald]].val + 1
-	for i in range(4) :
-		if(temp & 1 == 1) :
-			ac = ac + 1
-		temp = temp >> 1	
-	if(ac == 4) :
-		flags.b.ac = 1
-		print "Auxiallary Carry ocuured" 						
- 	
- 	if(lookup3[vald] < 10) : 		
-		reg[lookup3[vald]].update()
-	else :
-		reg[lookup3[vald]].valoverride(reg[lookup3[vald]].val)	
+	if(reg[lookup3[vald]].val == 127 or reg[lookup3[vald]].val == 32767 or 
+		reg[lookup3[vald]].val == 65536) :
+		flags.b.overflow = 1	
 	
-	return
-	
+		
 def fdiv(source):
 	a = 17							#cite! make ax from ah and al for every update of ah and al
 	(vals, modes) = extract(source)
@@ -326,147 +337,75 @@ def fdiv(source):
 	temp = reg[a].val
 	reg[2].val = temp / reg[lookup3[vals]].val
 	reg[1].val = temp % reg[lookup3[vals]].val
+	updatedep(1)
 	return
-	#AX, AL
-	#al, bl -> ax! how to map?
 	
 def fsub(dest, source):
 	(vald, moded) = extract(dest)
 	(vals, modes) = extract(source)
 	#destination is memory, do later
 	if(modes == 0 and moded == 0):
-		reg[lookup3[vald]].val -= reg[lookup3[vals]].val	#reg[lookup3[vald]] -
-		#we can maintain a list of values instead of a list of variables	
-	#if(moded == 1):		#bro, 
-		#pass
+		reg[lookup3[vald]].val -= reg[lookup3[vals]].val
 	if(modes == 1 and moded == 0):
 		reg[lookup3[vald]].val -= vals	#reg[lookup3[vald]] -
-	
-	
-	#Zero
-	if(reg[lookup3[vald]].val == 0) :
-		flags.b.zero = 1
-		print "The value in reg is zero"	
-	
-	temp_0 = reg[lookup3[vald]].val
-	c = 0
-	ac = 0
-	while (temp_0 > 0) :
-		if(temp_0 & 1 == 1):
-			c = c + 1
-		temp_0 = temp_0 >> 1
-	if(c % 2 == 0) :
-		flags.b.parity = 1
-		print "Parity even so Parity bit set to 1", flags.b.parity
-					
+
 	if(modes == 1 and moded == 0):					
 		temp = reg[lookup3[vald]].val + vals
 	else:
-		temp = reg[lookup3[vald]].val + reg[lookup3[vals]].val
-	
+		temp = reg[lookup3[vald]].val + reg[lookup3[vals]].val		#Akshay G
 	temp_0 = reg[lookup3[vald]].val
-	
 	if(lookup3[vald] < 10):	
-		if((temp > 127  and temp_0 < 127) or (temp > 0 and temp < 127 and temp_0 < 0)):
+		if((temp > 127  and temp_0 <= 127) or (temp > 0 and temp <= 127 and temp_0 < 0)):
 			flags.b.overflow = 1
-			print "Overflow occured"
 	else:
 		if((temp > 32767  and temp_0 <= 32767) or (temp >= 0 and temp <= 32767 and ((temp_0 < 0) or temp_0 > 32767))
 			or (temp > 32767 and temp_0 < 0)) :
-			flags.b.overflow = 1
-			print "Overflow Ocuured"
-	
-	if(temp >= 0 and temp_0 < 0):
-			flags.b.carry = 1
-			print "Carry generated"		
-			
-			
-	for i in range(4) :
-		if(temp & 1 == 1) :
-			ac = ac + 1
-		temp = temp >> 1	
-	if(ac == 4) :
-		flags.b.ac = 1
-		print "Auxiallary Carry ocuured"
-	
-	if(lookup3[vald] < 10):
+			flags.b.overflow = 1	
+	if(lookup3[vald] < 13):
 		reg[lookup3[vald]].update()
+		updatedep(lookup3[vald])
 	else:
 		reg[lookup3[vald]].valoverride(reg[lookup3[vald]].val)	
-		 
+	if(lookup3[vald] > 16):
+		updateacflag(reg[lookup3[vald]].val)	 
 		
 def fadd(dest, source) :
 	(vald, moded) = extract(dest)
 	(vals, modes) = extract(source)
 	#destination is memory, do later
 	if(modes == 0 and moded == 0):
-		reg[lookup3[vald]].val += reg[lookup3[vals]].val	#reg[lookup3[vald]] -
-		#we can maintain a list of values instead of a list of variables	
-	#if(moded == 1):		#bro, 
-		#pass
+		reg[lookup3[vald]].val += reg[lookup3[vals]].val
 	if(modes == 1 and moded == 0):
-		reg[lookup3[vald]].val += vals	#reg[lookup3[vald]] -
-	
-	
-	temp_0 = reg[lookup3[vald]].val
+		reg[lookup3[vald]].val += vals
+
+	temp_f = reg[lookup3[vald]].val		#Akshay G
 	c = 0
 	ac = 0
-	
-					
 	if(modes == 1 and moded == 0):					
 		temp = reg[lookup3[vald]].val - vals
 	else:
 		temp = reg[lookup3[vald]].val - reg[lookup3[vals]].val
 	
-	temp_0 = reg[lookup3[vald]].val
-	
-	
-	#FOr Overflow And Carry
+	temp_f = reg[lookup3[vald]].val
 	if(lookup3[vald] < 10) :
-		if(((temp > 127) and (temp_0 < 127)) or ((temp < 127) and (temp_0 > 127))) :
+		if(((temp > 127) and (temp_f <= 127)) or ((temp <= 127) and (temp_f > 127))) :
 			flags.b.overflow = 1
-			print "Overflow occured"
-		if(temp_0 > 255):
+		if(temp_f > 255):
 			flags.b.carry = 1
-			print "Carry generated"	
 	else:
-		if(((temp > 32767 and temp) and (temp_0 > 65535)) or ((temp < 32767) and (temp_0 > 32767))) :
+		if(((temp > 32767 and temp_f <= 32767)) or ((temp <= 32767) and (temp_f > 32767))) :
 			flags.b.overflow = 1
-			print "Overflow occured"
-		if(temp_0 > 65536):
+		if(temp_f > 65536):
 			flags.b.carry  = 1
-			print "Carry Generated"
 			
-	
-	
 	if(lookup3[vald] < 10):
 		reg[lookup3[vald]].update()
+		updatedep(lookup3[vald])
 	else:
 		reg[lookup3[vald]].valoverride(reg[lookup3[vald]].val)	
-	
-	#FOr Zerp			
-	if(temp_0 == 0):
-		flags.b.zero = 1
-		#PRint here	
-	
-	temp_0 = reg[lookup3[vald]].val	
-	while (temp_0 > 0) :
-		if(temp_0 & 1 == 1):
-			c = c + 1
-		temp_0 = temp_0 >> 1
-	if(c % 2 == 0) :
-		flags.b.parity = 1
-		print "Parity even so Parity bit set to 1", flags.b.parity	
-	
-	for i in range(4) :
-		if(temp & 1 == 1) :
-			ac = ac + 1
-		temp = temp >> 1	
-	if(ac == 4) :
-		flags.b.ac = 1
-		print "Auxiallary Carry ocuured"	
-		 				 	
-	
+		 	
+	if(lookup3[vald] > 16):
+		updateacflag(reg[lookup3[vald]].val)
 	
 				
 		
--- state.py
+++ state.py
@@ -34,37 +34,94 @@ class var_8(object):
 	def __init__(self):
 		self.val = 00
 	def update(self):
-		if(self.val > 0):
+		if(self.val > 255):
 			while(self.val > 255):
 				self.val -= 256
-		else: 
-			while(self.val < 0):
-				self.val += 256		
+			flags.b.carry = 1
+			flags.b.sign = 0
+			flags.b.zero = 0
+		elif(self.val < 0):
+			flags.b.sign = 1
+			flags.b.carry = 1 #Update #1
+			flags.b.zero = 0
+			self.val += 256
+		elif(self.val == 0):
+			flags.b.zero = 1
+			flags.b.sign = 0
+			flags.b.carry = 0	#sign, zero, carry, parity | overflow
+		c = 0
+		temp = self.val
+		while (temp > 0):
+			if(temp & 1 == 1):
+				c = c + 1
+			temp = temp >> 1
+		if(c % 2 == 0 and c != 0):
+			flags.b.parity = 1
+		else:
+			flags.b.parity = 0
 class var_16(object):
 	def __init__(self):
 		self.val = 0000
 	def update(self):
-		if(self.val > 0) :
+		if(self.val > 0):
 			while(self.val > 65535):
 				self.val -= 65536
-		else : 
-			while(self.val < 0):
-				self.val += 65536		
+			flags.b.carry = 1
+			flags.b.sign = 0
+			flags.b.zero = 0
+		elif(self.val < 0):
+			self.val += 65536		#store in 2'sc?
+			flags.b.carry = 1 #Update #1
+			flags.b.sign = 1
+			flags.b.zero = 0
+		elif(self.val == 0):
+			flags.b.carry = 0
+			flags.b.sign = 0
+			flags.b.zero = 1
+		c = 0
+		temp = self.val
+		while (temp > 0):
+			if(temp & 1 == 1):
+				c = c + 1
+			temp = temp >> 1
+		if(c % 2 == 0 and c != 0):
+			flags.b.parity = 1
+		else:
+			flags.b.parity = 0		
 
 class special_var(object):
 	def __init__(self, order):
 		self.val = 0000
 		self.order = order
-	def update(self):
+	def update(self):		
 		self.val = int(hex(reg[self.order].val)[2:] + hex(reg[self.order + 1].val)[2:], 16)
 	def valoverride(self, value):
 		self.val = value
 		if(self.val > 0):
 			while(self.val > 65535):
 				self.val -= 65536
-		else :
-			while(self.val < 0) :
-				self.val += 65536		
+			flags.b.carry = 1
+			flags.b.sign = 0
+			flags.b.zero = 0
+		elif(self.val < 0):
+			self.val += 65536
+			flags.b.carry = 1 #Update #1
+			flags.b.sign = 1
+			flags.b.zero = 0
+		elif(self.val == 0):
+			flags.b.carry = 0
+			flags.b.sign = 0
+			flags.b.zero = 1
+		c = 0
+		temp = self.val
+		while (temp > 0):
+			if(temp & 1 == 1):
+				c = c + 1
+			temp = temp >> 1
+		if(c % 2 == 0 and c != 0):
+			flags.b.parity = 1
+		else:
+			flags.b.parity = 0
 		if(self.val > 255):
 			reg[self.order].val = int(hex(self.val)[2:-2], 16)
 			reg[self.order + 1].val = int(hex(self.val)[-2:], 16)
